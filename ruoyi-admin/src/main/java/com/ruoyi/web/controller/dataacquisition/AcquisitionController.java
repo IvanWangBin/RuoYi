1fa97345f63f60833a93fedd2e38e9abd7600549
wangbin <ivanskywang@gmail.com>
2025-03-25 00:05:19 +0800

    1、数据采集模块init

---
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/dataacquisition/AcquisitionController.java
@@ -0,0 +1,23 @@
+package com.ruoyi.web.controller.dataacquisition;
+
+import com.ruoyi.common.core.controller.BaseController;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author wangbin
+ * @date 2025/3/24 23:47
+ */
+@Controller
+@RequestMapping("/data-acquisition/acquisition")
+public class AcquisitionController extends BaseController {
+    private String prefix = "data-acquisition/acquisition";
+
+    @RequiresPermissions("data-acquisition:acquisition:view")
+    @GetMapping()
+    public String menu() {
+        return prefix + "/acquisition";
+    }
+}
